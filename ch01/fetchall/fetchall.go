aec8430d059da27567fabed0e19619f7be4bfa22
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2024-06-14 20:51:01 -0300

    fetchall code

---
+++ ch01/fetchall/fetchall.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http") {
+			url = "https://" + url
+		}
+		go fetch(url, ch)
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+	nbytes, err := io.Copy(io.Discard, resp.Body)
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d kb %s", secs, nbytes/1024, url)
+}
