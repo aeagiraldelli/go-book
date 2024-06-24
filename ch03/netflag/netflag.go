5baebb66163920069fc9e7daf0beccd27f1c9214
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2024-06-24 16:37:08 -0300

    book page 119.

---
+++ ch03/netflag/netflag.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type Flags uint
+
+const (
+	FlagUp Flags = 1 << iota
+	FlagBroadcast
+	FlagLoopback
+	FlagPointToPoint
+	FlagMulticast
+)
+
+const (
+	_ = 1 << (10 * iota)
+	KiB
+	MiB
+)
+
+func IsUp(v Flags) bool     { return v&FlagUp == FlagUp }
+func TurnDown(v *Flags)     { *v &^= FlagUp }
+func SetBroadcast(v *Flags) { *v |= FlagBroadcast }
+func IsCast(v Flags) bool   { return v&(FlagBroadcast|FlagMulticast) != 0 }
+
+func main() {
+	v := FlagMulticast | FlagUp
+	fmt.Printf("%b %t\n", v, IsUp(v)) // "10001 true"
+	TurnDown(&v)
+	fmt.Printf("%b %t\n", v, IsUp(v)) // "10000 false"
+	SetBroadcast(&v)
+	fmt.Printf("%b %t\n", v, IsUp(v))   // "10010 false"
+	fmt.Printf("%b %t\n", v, IsCast(v)) // "10010 true"
+}
