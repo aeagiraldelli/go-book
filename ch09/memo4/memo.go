aeee518b8f5572566201401811fb35e77d550bad
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2024-08-13 21:14:34 -0300

    book page: 373.
    finish chapter 09.

---
+++ ch09/memo4/memo.go
@@ -0,0 +1,47 @@
+package memo4
+
+import "sync"
+
+type result struct {
+	value interface{}
+	err   error
+}
+
+type Func func(key string) (interface{}, error)
+
+type entry struct {
+	res   result
+	ready chan struct{}
+}
+
+func New(f Func) *Memo {
+	return &Memo{f: f, cache: make(map[string]*entry)}
+}
+
+type Memo struct {
+	f     Func
+	mu    sync.Mutex
+	cache map[string]*entry
+}
+
+func (memo *Memo) Get(key string) (value interface{}, err error) {
+	memo.mu.Lock()
+	e := memo.cache[key]
+	if e == nil {
+		// This is the first request for this key.
+		// This goroutine becomes responsible for computing
+		// the value and broadcasting the ready condition.
+		e = &entry{ready: make(chan struct{})}
+		memo.cache[key] = e
+		memo.mu.Unlock()
+
+		e.res.value, e.res.err = memo.f(key)
+
+		close(e.ready) // broadcast ready condition
+	} else {
+		// This is a repeat request for this key.
+		memo.mu.Unlock()
+		<-e.ready
+	}
+	return e.res.value, e.res.err
+}
